cf27c1b60371d6ddade34a033377ed2bfc0e32a8
uc02 <upendrachauhan0206@gmail.com>
2025-03-23 19:58:31 +0530

    first commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="" className="home social-icon" target="_blank"></a>
+
+      <a href="" className="home social-icon" target="_blank"></a>
+
+      <a href="" className="home social-icon" target="_blank"></a>
+
+    </div>
+  )
+}
+
+export default Social
