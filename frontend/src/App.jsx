40aa26a11ced58cac74aebd876214a0abd186a4b
uc02 <upendrachauhan0206@gmail.com>
2025-04-30 09:29:20 +0530

    Adding Loading Functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Hero from './components/Hero'
 import Services from './components/Services'
 import Contact from './components/Contact'
+import Loader from './components/Loader'
 import { ThemeProvider } from './context/ThemeContext'
 
 const App = () => {
+
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+   const timer = setTimeout(() => {
+      setIsLoading(false)
+    },2000)
+
+    return () => clearTimeout(timer)
+  },[])
+
   return (
     <ThemeProvider>
      <div className='min-h-screen bg-white dark:bg-gray-900 text-red-500 dark:text-yellow-500 transition-colors duration-500'>
-       <Hero/>
-       <Services/>
-       <Contact/>
+       <Loader isLoading={isLoading}/>
+       {!isLoading && (
+        <>
+        <Hero/>
+        <Services/>
+        <Contact/>
+        </>
+       )}
+       
      </div>
     </ThemeProvider>
   )
